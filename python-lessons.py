063c073bf84268898f00dc9af3ff3c20783b1297
Lazykery <dogadrom@gmail.com>
2026-07-09 04:17:34 +0300

    ya

--- python-lessons.py
+++ python-lessons.py
@@ -1,5 +1,17 @@
-print(1)
-print(12)
-print(123)
-for i in range(5):
-    print(1324343)
+number = int(input())
+a_word = str(input())
+a_number = int(input())
+while number > 1:
+    acont = 0
+    bcont = 0
+    b_word = str(input())
+    b_number = int(input())
+    for i in str(a_number):
+        acont += int(i)
+    for ii in str(b_number):
+        bcont += int(ii)
+    if bcont >= acont:
+        a_word = b_word
+        a_number = b_number
+    number -= 1
+print(a_word)
